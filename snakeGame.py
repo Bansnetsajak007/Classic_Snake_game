13bf961147247c8f81e445698a58e45c3798042e
sajak <sajakbasnet77@gmail.com>
2023-10-04 08:18:01 +0545

    updated display board and added generate food

--- snakeGame.py
+++ snakeGame.py
@@ -3,26 +3,46 @@
 #work in progress
 
 #Iam assuming here i will be wasting 6-7 hours of my life
-
-def gameBoard():
-    size = 10
-    for i in range(0,size):
-        for j in range(0,size):
-            if(i == 0 or i == size - 1 or j == 0 or j == size - 1):
-                print("0  ",end="")
-            else:
-                print("   ",end="")
-                
-        print("\n")
-
+import random
 
 def displayScore():
     score = 100
     print(f"\nscore:{score}")
     print()
 
+def displayBoard(board):
+    for row in board:
+        print(' '.join(row))
+
+
+def gameBoard(size = 30):
+    #formatted method more python type
+    board = [[' ' for _ in range(size)] for _ in range(size)]
+
+    for i in range(size):
+        board[i][0] = 'H'
+        board[i][size -1 ] = 'H'
+        board[0][i] = 'H'
+        board[size - 1][i] = 'H'
+
+    return board
+
+
+def generateFood(board):
+    totalSize = len(board)
+
+    #Generating food randomly using coordinates
+    while True:
+        row = random.randint(1, totalSize - 2)
+        column = random.randint(1, totalSize - 2)
+
+        if(board[row][column] == ' '):
+            board[row][column] = 'F' #displaying food as F
+            break
 
 if __name__ == "__main__":
 
     displayScore()
-    gameBoard()
+    board = gameBoard()
+    generateFood(board)
+    displayBoard(board)
